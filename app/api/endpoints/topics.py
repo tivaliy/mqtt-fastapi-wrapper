d1e4f73fbfab2ce08d57389843f08e5b557ae4fe
Vitalii Kulanov <vkulanov@waverleysoftware.com>
2021-11-14 13:46:38 +0200

    Revisit /topics endpoint implementation by adding fakes

--- app/api/endpoints/topics.py
+++ app/api/endpoints/topics.py
@@ -1,23 +1,42 @@
-from fastapi import APIRouter
+import uuid
+from datetime import datetime
+
+from fastapi import APIRouter, Body, HTTPException
 
 from app.mqtt.client import fast_mqtt
 
 
 router = APIRouter()
 
+# fake topic list
+topic_list = ['foo', 'bar']
+
 
 @router.get("/")
 def read_topics():
-    return {
-        "message": "Welcome to MQTT topics list"
-    }
+    # stub topic list response
+    topic_list_response = [
+        {
+            "id": uuid.uuid4(),
+            "name": name,
+            "created_at": datetime.now()
+        }
+        for name in topic_list
+    ]
+    return topic_list_response
 
 
 @router.post("/{topic_name}")
-def publish_to_topics(topic_name: str):
+def publish_to_topics(
+        topic_name: str,
+        qos: int = 0,
+        message: str = Body(..., embed=True)
+):
 
-    fast_mqtt.publish(f"/{topic_name}", "Hello from Fastapi")
+    if topic_name not in topic_list:
+        raise HTTPException(status_code=404, detail=f"Topic not found")
 
-    return {
-        "message": f"Success - published to {topic_name}"
-    }
+    fast_mqtt.publish(f"/{topic_name}", message, qos=qos)
+
+    # 200 Ok with empty body
+    return {}
