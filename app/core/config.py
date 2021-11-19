d5efc58e0d17bcd9ffa6eff74d294430b5a7b4f4
Vitalii Kulanov <vkulanov@waverleysoftware.com>
2021-11-19 09:14:23 +0200

    Add development/production settings

--- app/core/config.py
+++ app/core/config.py
@@ -2,13 +2,14 @@ from functools import lru_cache
 from typing import Dict, Type
 
 from app.core.settings.test import TestAppSettings
+from app.core.settings.development import DevAppSettings
+from app.core.settings.production import ProdAppSettings
 from app.core.settings.base import BaseAppSettings, AppEnvTypes
 
 
 environments: Dict[AppEnvTypes, Type[BaseAppSettings]] = {
-    # TODO: Implement settings for Dev and Prod
-    # AppEnvTypes.dev: DevAppSettings
-    # AppEnvTypes.prod: ProdAppSettings
+    AppEnvTypes.dev: DevAppSettings,
+    AppEnvTypes.prod: ProdAppSettings,
     AppEnvTypes.test: TestAppSettings
 }
 
